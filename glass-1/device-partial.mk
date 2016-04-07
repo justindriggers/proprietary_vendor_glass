2894ccd832aa040a796032693ccbd45cf056d55b
Justin Driggers <justin@justindriggers.com>
2016-04-06 22:36:13 -0400

    Add proprietary blobs from XE22

---
+++ glass-1/device-partial.mk
@@ -0,0 +1,65 @@
+#
+# Copyright (C) 2016 Justin Driggers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PRODUCT_PACKAGES := \
+    audio.primary.glass_1 \
+    gps.omap4 \
+    gralloc.omap4430 \
+    libbltsville_gc2d \
+    libbltsville_ticpu \
+    sensors.glass_1
+
+PRODUCT_COPY_FILES := \
+    vendor/glass/glass-1/proprietary/bin/bqflash:system/bin/bqflash \
+    vendor/glass/glass-1/proprietary/bin/raft:system/bin/raft \
+    vendor/glass/glass-1/proprietary/bin/shm_service:system/bin/shm_service \
+    vendor/glass/glass-1/proprietary/bin/sysproxy:system/bin/sysproxy \
+    vendor/glass/glass-1/proprietary/bin/sysproxyd:system/bin/sysproxyd \
+    vendor/glass/glass-1/proprietary/lib/modules/pvrsrvkm_sgx540_120.ko:system/lib/modules/pvrsrvkm_sgx540_120.ko \
+    vendor/glass/glass-1/proprietary/lib/modules/pvrsrvkm_sgx544_112.ko:system/lib/modules/pvrsrvkm_sgx544_112.ko \
+    vendor/glass/glass-1/proprietary/vendor/bin/pvrsrvctl_SGX540_120:system/vendor/bin/pvrsrvctl_SGX540_120 \
+    vendor/glass/glass-1/proprietary/vendor/bin/pvrsrvctl_SGX544_112:system/vendor/bin/pvrsrvctl_SGX544_112 \
+    vendor/glass/glass-1/proprietary/vendor/bin/pvrsrvinit:system/vendor/bin/pvrsrvinit \
+    vendor/glass/glass-1/proprietary/vendor/firmware/bcm4330.hcd:system/vendor/firmware/bcm4330.hcd \
+    vendor/glass/glass-1/proprietary/vendor/firmware/ducati-m3-core0.xem3:system/vendor/firmware/ducati-m3-core0.xem3 \
+    vendor/glass/glass-1/proprietary/vendor/firmware/ducati-m3-core0-2gb.xem3:system/vendor/firmware/ducati-m3-core0-2gb.xem3 \
+    vendor/glass/glass-1/proprietary/vendor/firmware/fw_bcmdhd.bin:system/vendor/firmware/fw_bcmdhd.bin \
+    vendor/glass/glass-1/proprietary/vendor/firmware/glasshub.s19:system/vendor/firmware/glasshub.s19 \
+    vendor/glass/glass-1/proprietary/vendor/firmware/NVM2:system/vendor/firmware/NVM2 \
+    vendor/glass/glass-1/proprietary/vendor/firmware/tesla-dsp.xe64T:system/vendor/firmware/tesla-dsp.xe64T \
+    vendor/glass/glass-1/proprietary/vendor/lib/libglslcompiler_SGX540_120.so:system/vendor/lib/libglslcompiler_SGX540_120.so \
+    vendor/glass/glass-1/proprietary/vendor/lib/libglslcompiler_SGX544_112.so:system/vendor/lib/libglslcompiler_SGX544_112.so \
+    vendor/glass/glass-1/proprietary/vendor/lib/libIMGegl_SGX540_120.so:system/vendor/lib/libIMGegl_SGX540_120.so \
+    vendor/glass/glass-1/proprietary/vendor/lib/libIMGegl_SGX544_112.so:system/vendor/lib/libIMGegl_SGX544_112.so \
+    vendor/glass/glass-1/proprietary/vendor/lib/libpvr2d_SGX540_120.so:system/vendor/lib/libpvr2d_SGX540_120.so \
+    vendor/glass/glass-1/proprietary/vendor/lib/libpvr2d_SGX544_112.so:system/vendor/lib/libpvr2d_SGX544_112.so \
+    vendor/glass/glass-1/proprietary/vendor/lib/libpvrANDROID_WSEGL_SGX540_120.so:system/vendor/lib/libpvrANDROID_WSEGL_SGX540_120.so \
+    vendor/glass/glass-1/proprietary/vendor/lib/libpvrANDROID_WSEGL_SGX544_112.so:system/vendor/lib/libpvrANDROID_WSEGL_SGX544_112.so \
+    vendor/glass/glass-1/proprietary/vendor/lib/libPVRScopeServices_SGX540_120.so:system/vendor/lib/libPVRScopeServices_SGX540_120.so \
+    vendor/glass/glass-1/proprietary/vendor/lib/libPVRScopeServices_SGX544_112.so:system/vendor/lib/libPVRScopeServices_SGX544_112.so \
+    vendor/glass/glass-1/proprietary/vendor/lib/libsrv_init_SGX540_120.so:system/vendor/lib/libsrv_init_SGX540_120.so \
+    vendor/glass/glass-1/proprietary/vendor/lib/libsrv_init_SGX544_112.so:system/vendor/lib/libsrv_init_SGX544_112.so \
+    vendor/glass/glass-1/proprietary/vendor/lib/libsrv_um_SGX540_120.so:system/vendor/lib/libsrv_um_SGX540_120.so \
+    vendor/glass/glass-1/proprietary/vendor/lib/libsrv_um_SGX544_112.so:system/vendor/lib/libsrv_um_SGX544_112.so \
+    vendor/glass/glass-1/proprietary/vendor/lib/libusc_SGX540_120.so:system/vendor/lib/libusc_SGX540_120.so \
+    vendor/glass/glass-1/proprietary/vendor/lib/libusc_SGX544_112.so:system/vendor/lib/libusc_SGX544_112.so \
+    vendor/glass/glass-1/proprietary/vendor/lib/egl/libEGL_POWERVR_SGX540_120.so:system/vendor/lib/egl/libEGL_POWERVR_SGX540_120.so \
+    vendor/glass/glass-1/proprietary/vendor/lib/egl/libEGL_POWERVR_SGX544_112.so:system/vendor/lib/egl/libEGL_POWERVR_SGX544_112.so \
+    vendor/glass/glass-1/proprietary/vendor/lib/egl/libGLESv1_CM_POWERVR_SGX540_120.so:system/vendor/lib/egl/libGLESv1_CM_POWERVR_SGX540_120.so \
+    vendor/glass/glass-1/proprietary/vendor/lib/egl/libGLESv1_CM_POWERVR_SGX544_112.so:system/vendor/lib/egl/libGLESv1_CM_POWERVR_SGX544_112.so \
+    vendor/glass/glass-1/proprietary/vendor/lib/egl/libGLESv2_POWERVR_SGX540_120.so:system/vendor/lib/egl/libGLESv2_POWERVR_SGX540_120.so \
+    vendor/glass/glass-1/proprietary/vendor/lib/egl/libGLESv2_POWERVR_SGX544_112.so:system/vendor/lib/egl/libGLESv2_POWERVR_SGX544_112.so \
+    vendor/glass/glass-1/proprietary/xbin/bootconfig:system/xbin/bootconfig
---
+++ glass-1/proprietary/Android.mk
@@ -0,0 +1,139 @@
+#
+# Copyright (C) 2016 Justin Driggers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),glass-1)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE := audio.primary.glass_1
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_SRC_FILES := lib/hw/audio.primary.glass_1.so
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE := gps.omap4
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_SRC_FILES := lib/hw/gps.omap4.so
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE := sensors.glass_1
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_SRC_FILES := lib/hw/sensors.glass_1.so
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE := gralloc.omap4430
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_SRC_FILES := vendor/lib/hw/gralloc.omap4430.so
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/lib/hw
+
+include $(BUILD_PREBUILT)
+
+SYMLINKS := $(TARGET_OUT_VENDOR)/lib/hw/gralloc.omap4.so
+$(SYMLINKS): GRALLOC_BINARY := ./gralloc.omap4430.so
+$(SYMLINKS): $(LOCAL_INSTALLED_MODULE) $(LOCAL_PATH)/Android.mk
+	@echo "Symlink: $@ -> $(GRALLOC_BINARY)"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -fs $(GRALLOC_BINARY) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(SYMLINKS)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE := libbltsville_ticpu
+LOCAL_MODULE_SUFFIX := .2.1.0.0.so
+LOCAL_SRC_FILES := \
+    vendor/lib/libbltsville_ticpu.2.1.0.0.so \
+    vendor/lib/libbltsville_ticpu_license.txt
+LOCAL_MODULE_PATH:= $(TARGET_OUT_VENDOR)/lib
+
+include $(BUILD_PREBUILT)
+
+SYMLINKS := $(TARGET_OUT_VENDOR)/lib/libbltsville_ticpu.so
+$(SYMLINKS): TICPU_BINARY := ./libbltsville_ticpu.2.1.0.0.so
+$(SYMLINKS): $(LOCAL_INSTALLED_MODULE) $(LOCAL_PATH)/Android.mk
+	@echo "Symlink: $@ -> $(TICPU_BINARY)"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -fs $(TICPU_BINARY) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(SYMLINKS)
+
+SYMLINKS1 := $(TARGET_OUT_VENDOR)/lib/libbltsville_cpu.so
+$(SYMLINKS1): LINK_BINARY := ./libbltsville_ticpu.so
+$(SYMLINKS1): $(LOCAL_INSTALLED_MODULE) $(LOCAL_PATH)/Android.mk
+	@echo "Symlink: $@ -> $(LINK_BINARY)"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -fs $(LINK_BINARY) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(SYMLINKS1)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE := libbltsville_gc2d
+LOCAL_MODULE_SUFFIX := .2.2.2.0.so
+LOCAL_SRC_FILES := vendor/lib/libbltsville_gc2d.2.2.2.0.so
+LOCAL_MODULE_PATH:= $(TARGET_OUT_VENDOR)/lib
+
+include $(BUILD_PREBUILT)
+
+SYMLINKS := $(TARGET_OUT_VENDOR)/lib/libbltsville_gc2d.so
+$(SYMLINKS): GC2D_BINARY := ./libbltsville_gc2d.2.2.2.0.so
+$(SYMLINKS): $(LOCAL_INSTALLED_MODULE) $(LOCAL_PATH)/Android.mk
+	@echo "Symlink: $@ -> $(GC2D_BINARY)"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -fs $(GC2D_BINARY) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(SYMLINKS)
+
+SYMLINKS1 := $(TARGET_OUT_VENDOR)/lib/libbltsville_hw2d.so
+$(SYMLINKS1): LINK_BINARY := ./libbltsville_gc2d.so
+$(SYMLINKS1): $(LOCAL_INSTALLED_MODULE) $(LOCAL_PATH)/Android.mk
+	@echo "Symlink: $@ -> $(LINK_BINARY)"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -fs $(LINK_BINARY) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(SYMLINKS1)
+
+endif
